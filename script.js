b4c85936da6a1d2a51578b333dd2dcc43ae525e1
MaX <svtsk.mv@gmail.com>
2020-04-07 15:12:34 +0300

    3d lesson

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start(){
+    money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+    while(isNaN(money) || money == "" || money == null ) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+  
+    }
+}
+
+start();
     
 let appData = {
     budget: money,
@@ -7,27 +18,32 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: false
+    savings: true
 };
 
+function chooseExpenses(){
 
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
     
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-
-    if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '') {
-        console.log('done');
-
-        appData.expenses[a] = b;
-
-    } else {
-
+        if((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '') {
+            console.log('done');
+    
+            appData.expenses[a] = b;
+    
+        } else {
+    
+        }
+        
     }
-    
-};
+}
 
-appData.moneyPerday = appData.budget/30;
+chooseExpenses();
+    
+
+
+appData.moneyPerday = (appData.budget/30).toFixed(1);
 
 alert("Ваш ежедневывный бюджет " + appData.budget);
 
@@ -41,3 +57,16 @@ if(appData.budget < 100 ){
     console.log('Ошибка!');
 } 
 
+function checkSavings() {
+
+    if (appData.savings == true) {
+        let save = +prompt("Какая сумма накоплений?"),
+            percent = +prompt("А под какой процент?");
+
+        appData.monthIncome = save/100/12 * percent;
+        alert("Ежемесячный доход ровняется " + appData.monthIncome);
+
+    }
+}
+
+checkSavings();
